68a0925a73649749a5095802bc7e26e8e5e8f28b
User <user@Users-Mac-mini.local>
2021-12-29 14:15:51 +0600

    starting of setup

---
+++ youtube-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-demo
+//
+//  Created by User on 29/12/21.
+//  Copyright © 2021 User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
